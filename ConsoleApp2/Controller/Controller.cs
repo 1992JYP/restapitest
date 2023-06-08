451665b7e325969d5ad5334cb77f0e2b90fcac94
EGAP\202212002 <202212002@egap.co.kr>
2023-06-08 09:39:31 +0900

    컨트롤러 분리 방법 고민중

---
+++ ConsoleApp2/Controller/Controller.cs
@@ -0,0 +1,40 @@
+﻿using EGIO_DOTNET.API.seibro.bond;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace EGIO_DOTNET.Controller
+{
+    internal class Controller
+    {
+        public Controller() { }
+        public async void Fn_Contol()
+        {
+            Bondinfo bondinfo1 = new("20230531");
+
+            RestClient restClient = bondinfo1.Fn_RestClient();
+
+            Console.WriteLine(bondinfo1.Fn_returnUrl());
+
+            RestRequest request = new RestRequest();
+            RestResponse response = await restClient.GetAsync(request);
+            Console.WriteLine(response.Content + "\n\n\n\n\n\n");
+
+            XmlDocument xml = new XmlDocument();
+            xml.LoadXml(response.Content);
+
+            XmlNodeList xmlNodeList = xml.GetElementsByTagName("ISIN");
+
+            int i = 0;
+            foreach (XmlNode xmlNode in xmlNodeList)
+            {
+                Console.WriteLine(i++);
+                Console.WriteLine(xmlNode.Attributes["value"].Value);
+            }
+        }
+    }
+}
